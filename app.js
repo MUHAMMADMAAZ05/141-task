9a56892e28a328298e3fad817c533339c67504d7
Muhammad Maaz Shaikh <shaikhsiddiqui931@gmail.com>
2025-02-28 17:09:32 -0800

    Background Colour Changer

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-let bgcolor=["Black","Orange","Green"]
+const colors = ["#F1f5f8", "#FF5733", "#33FF57", "#3357FF", "#FF33A6", "#FF8C33", "#FFD133"];
+const colorCode = document.getElementById("colorCode");
+const colorButton = document.getElementById("colorButton");
 
-document.body.style.backgroundColor= bgcolor
+colorButton.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  console.log(randomIndex);
+  
+  const selectedColor = colors[randomIndex];
+  console.log(selectedColor);
+  
+  document.body.style.backgroundColor = selectedColor;
+  colorCode.innerHTML = `Background Color: <span>${selectedColor}</span>`;
+});
